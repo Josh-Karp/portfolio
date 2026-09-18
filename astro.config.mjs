b3488a87f1579ced76946f3cc1523caca41bbaee
Joshua Karp <joshua@taskflow.co.za>
2026-09-18 15:59:09 +0200

    fix: rectify tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
